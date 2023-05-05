dc685b2ee6c0fd8467361dbe86e3be32c1cac94c
BenHbrt <benherbert1983@googlemail.com>
2023-05-05 21:20:17 +0200

    Concurrently and react code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.scss';
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const loadData = async () => {
+    const APIData = await axios.get('http://localhost:3001/api/demoNames')
+    setData(APIData.data.data)
+  }
+
+  useEffect(() => {
+    loadData()
+  })
+
   return (
     <div className="App">
-      Fullstack Boilerplate
+      <h1>Fullstack Boilerplate</h1>
+      <p><strong>Demo DB data:</strong></p>
+      {
+        data ? 
+        <ul>
+          {
+            data.map((item) => {
+              return <li>ID: {item.id}, Name: {item.name}</li>
+            })
+          }
+        </ul>
+        :
+        null
+      }
     </div>
   );
 }
